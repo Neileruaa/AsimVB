87f7facec381b2aabc2019594364587dc978a88d
aurelien <aureliendrey@gmail.com>
2019-09-22 18:56:36 +0200

    test form #8

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,14 +12,25 @@ const SecondPage = () => (
             <p className="text-minor">Bienvenue sur la page contact de l'ASIM VolleyBall. N'hésitez pas à nous envoyer un message </p>
         </div>
 
-        <form
-            method="post"
-            name="contact"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-        >
-            <input type="hidden" name="bot-field"/>
-            <input type="email" name="userEmail" id="userEmail" placeholder="Votre email"/>
+        <form name="contact" method="POST" data-netlify="true">
+            <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <label>Your Role: <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                </select></label>
+            </p>
+            <p>
+                <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
         </form>
 
 
